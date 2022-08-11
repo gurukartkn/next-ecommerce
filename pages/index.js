895316a0f08a74a89d03236fbadfe93a0e0d62a9
Guru Karthikeyan <gurukartkn@gmail.com>
2022-08-11 17:16:12 +0530

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Dollar Smart</title>
+        <meta
+          name="description"
+          content="Dollar Smart is a fake e-commerce website created using Platzi Fake Store API"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1 className="text-xl font-bold font-mono">Dollar Smart</h1>
+      </main>
+    </div>
+  );
+}
